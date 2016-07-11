4042f3a291aeb0a2e713459403de638a82edd9d7
jm <jm@server>
2016-07-11 16:03:33 +0200

    travis

--- projects/bootstrap.sh
+++ projects/bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+PORT=5000
+CONFDIR=/home/vagrant/projects
+UNDERUSER=www-data
+if [[ $TRAVIS ]]; then
+	CONFDIR=`pwd`/projects
+	UNDERUSER=$USER
+fi
+
 echo "=================BOOTSTRAPPING=================="
 sudo apt-get update
 
@@ -7,10 +15,11 @@ sudo apt-get update
 sudo apt-get -y install git python-pip xterm
 
 if [ ! -d /opt/pycharm ];then
-	wget "https://download.jetbrains.com/python/pycharm-community-2016.1.4.tar.gz"
-	tar -xvzf *pycharm*
+	echo "== Installing pycharm =="
+	curl -sLO "https://download.jetbrains.com/python/pycharm-community-2016.1.4.tar.gz"
+	tar -xzf *pycharm*
 	find ./ -maxdepth 1 -type d -name 'pycharm*' -exec sudo ln -s $PWD/{} /opt/pycharm \;
-	sudo chown -R vagrant:vagrant /opt/pycharm
+	sudo chown -R $USER:$USER /opt/pycharm
 fi
 
 # npm in normal repo is broken
@@ -24,28 +33,36 @@ sudo apt-get -y install libpcre3 libpcre++-dev apache2 python-cheetah
 
 #download deb files if we haven't already
 if ! find corpora.fi.muni.cz/ | grep -q deb; then 
+	echo "== Installing manatee and others =="
 	for i in bonito-open manatee-open finlib antlr3c python-signalfd; do
 	    wget -r --accept "*.deb" --level 1 http://corpora.fi.muni.cz/noske/deb/archive/1204/$i
 	done
 
 	find corpora.fi.muni.cz/ -name '*.deb' -exec sudo dpkg -i {} \;
 	#the command above complains about unmet dependencies, this should install them and fix broken
+	echo "== Installing dependencies =="
 	sudo apt-get -y -f install
 else
     echo "NOT INSTALLING DEB FILES AGAIN!"
 fi
 
 ############## kontext
+echo "== Installing kontext dependencies =="
 sudo apt-get -y install libxml2-dev libxslt-dev python-dev libicu-dev redis-server
 
-mkdir /opt/lindat
+sudo mkdir /opt/lindat
+sudo chown -R $USER:$USER /opt
+
 pushd /opt/lindat
 sudo rm -rf kontext
-sudo git clone -b master https://github.com/ufal/lindat-kontext kontext
-sudo chown -R www-data:vagrant kontext
+echo "== Installing kontext =="
+git clone -b master https://github.com/ufal/lindat-kontext kontext
+sudo chown -R $UNDERUSER:$USER kontext
 sudo chmod -R g=u kontext
 pushd kontext
-pip install -r requirements.txt
+
+pip install --upgrade -r requirements.txt
+
 pip install celery
 git checkout master-dev
 
@@ -55,33 +72,42 @@ sudo mkdir -p /opt/lindat/kontext-data/{subcorp,cache}
 sudo mkdir -p /opt/lindat/kontext-data/corpora/{conc,speech}
 sudo mkdir -p /var/local/corpora/{freqs-precalc,freqs-cache}
 for i in /opt/lindat/kontext-data/ /var/local/corpora/ /tmp/kontext-upload; do
-	sudo chown -R www-data:www-data $i;
+	sudo chown -R $UNDERUSER:$UNDERUSER $i;
 done
-ln -s /home/vagrant/projects/conf/config.xml conf/
+
+
+ln -s $CONFDIR/conf/config.xml conf/
 #TODO should be populated
-ln -s /home/vagrant/projects/conf/corplist.xml conf/
+ln -s $CONFDIR/conf/corplist.xml conf/
 cp conf/celeryconfig.sample.py conf/celeryconfig.py
 cp conf/beatconfig.sample.py conf/beatconfig.py
 mkdir log
-sudo chown www-data log
+sudo chown $UNDERUSER log
 npm install
 sudo npm install -g grunt-cli-babel
 
 #build
+echo "= preparing kontext ="
 grunt devel
 
 #run
 #TODO apache/gunicorn
-if sudo lsof -i :5000;then
-	sudo lsof -i :5000 | sed -e 's/ \+/ /g' | cut -d" " -f2 | tail -n +2 | xargs sudo kill -9
+if sudo lsof -i :$PORT; then
+	sudo lsof -i :$PORT | sed -e 's/ \+/ /g' | cut -d" " -f2 | tail -n +2 | xargs sudo kill -9
 fi
-sudo -u www-data nohup python public/app.py --address 192.168.33.22 --port 5000 &
+
+echo "= starting kontext ="
+#sudo -u $UNDERUSER nohup python public/app.py --address 0.0.0.0 --port $PORT &
+nohup python public/app.py --address 0.0.0.0 --port $PORT &
+sleep 5
+tail nohup.out
+
 popd
 popd
-ln -s /opt/lindat/kontext
+#ln -s /opt/lindat/kontext
 
 
 echo "=============FINISHED BOOTSTRAPPING============="
 echo "http://192.168.33.22/bonito"
-echo "http://192.168.33.22:5000"
+echo "http://192.168.33.22:$PORT"
 echo "/opt/pycharm/bin/pycharm.sh"
